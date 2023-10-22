cab61d3ef69ede764a85924908d62a088a6ac102
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-10-21 21:47:29 -0400

    Update Credits.js

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -7,15 +7,7 @@ Note: You need to work on this file for the Assignment.
 import {Link} from 'react-router-dom';
 
 const Credits = (props) => {
-  handleAddCredit = (newCredit) => {
-  // Add the new credit to the list
-  const updatedCreditList = [...this.props.credits, newCredit];
-  this.setState({ creditList: updatedCreditList });
 
-  // Update the account balance
-  const newAccountBalance = this.props.calculateAccountBalance();
-  this.setState({ accountBalance: newAccountBalance });
-};
 
   return (
     <div>
