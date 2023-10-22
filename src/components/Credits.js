682b80042a1f1b93d3348ee42f3d1aecfff4f44d
Weilin Gao <136024634+WeiLinGao@users.noreply.github.com>
2023-10-21 22:01:51 -0400

    Update Credits.js

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -10,10 +10,11 @@ const Credits = (props) => {
 
   let creditsView = () => {
     const { credits } = props;
-    return credits.map(credit) => 
-      let date = credit.date.slice(0,10);
-      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
+    return credits.map((credit) => {
+      let date = credit.date.slice(0, 10);
+      return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>;
     });
+  };
 
   return (
     <div>
