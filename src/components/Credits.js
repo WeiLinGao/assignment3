6760ce27ade66ee94eb459a966797ec2d8e12a5e
weilin <weilingao829@gmail.com>
2023-10-21 22:31:31 -0400

    sd

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -9,7 +9,7 @@ import {Link} from 'react-router-dom';
 const Credits = (props) => {
   return (
     <div>
-      <h1>Credits</h1>
+      <h1>Creditsssssssssss</h1>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
